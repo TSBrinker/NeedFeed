9760d5a8fcc511505155b81a1dd106b473238373
TSBrinker <tsbrinker@gmail.com>
2023-02-18 18:12:57 -0600

    Reply model finished according to ERD

--- backend/reply/models.py
+++ backend/reply/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from post.models import Post
+from authentication.models import User
+import datetime
 
 # Create your models here.
+
+class Reply(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    body = models.CharField(max_length=100)
+    timestamp = models.DateField(default=datetime.date.today)
