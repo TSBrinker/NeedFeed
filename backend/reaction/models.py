7884e152bd59519563b482904b9b6db369bb63c5
TSBrinker <tsbrinker@gmail.com>
2023-02-18 18:14:59 -0600

    Final model created for Reaction app, moving on to serializers

--- backend/reaction/models.py
+++ backend/reaction/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from authentication.models import User
+from post.models import Post
 
 # Create your models here.
+
+user = models.ForeignKey(User, on_delete=models.CASCADE)
+post = models.ForeignKey(Post, on_delete=models.CASCADE)
